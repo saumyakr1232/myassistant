54e320cd177950e71730ad124049c79df48ec03d
saumya <saumyakr181999@gmail.com>
2021-04-24 20:36:33 +0530

    take notes --fix path

--- assist/note/note.py
+++ assist/note/note.py
@@ -1,19 +1,22 @@
 import datetime
-import subprocess
-import time
+from assist.utils.helper import  get_directory
 
 
 def note(text):
     date = datetime.datetime.now()
-    file_name = str(date).replace(":", "-") + "-note.txt"
-    with open(file_name, "w") as f:
+    directory = get_directory('notes')
+    path = directory / f"{text[:5]}-{date.timestamp()}-note.txt"
+
+    with open(path, "w") as f:
         f.write(text)
 
-    proc = subprocess.Popen(["gedit", file_name])
-    # proc.wait() #to wait until you close the gedit
-    time.sleep(5)
-    proc.kill()
+    # proc = subprocess.Popen(["gedit", file_name])
+    # # proc.wait() #to wait until you close the gedit
+    # time.sleep(5)
+    # proc.kill()
+
+    return f"Notes saved in {str(directory.absolute())}"
 
 
 if __name__ == '__main__':
-    note("alternate")
+    note("TEsting")
